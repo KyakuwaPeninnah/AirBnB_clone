47a0262a890feb2758c6c588ec1509a1b258dbcf
root <you@example.com>
2024-05-18 13:46:16 -0700

    Made changes on task 4

--- models/base_model.py
+++ models/base_model.py
@@ -14,6 +14,9 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """Initializing instance attributes"""
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.utcnow()
+        self.updated_at = datetime.utcnow()
         if kwargs:
             for key, value in kwargs.items():
                 if key != "__class__":
@@ -22,9 +25,6 @@ class BaseModel:
                             value, "%Y-%m-%dT%H:%M:%S.%f"))
                     else:
                         setattr(self, key, value)
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.utcnow()
-        self.updated_at = datetime.utcnow()
         models.storage.new(self)
 
     def __str__(self):
